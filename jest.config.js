7407ee8b1c69f67c77fed16ae4a923737c8a2664
Yuri Khrunyk <62480145+yurikhrunyk@users.noreply.github.com>
2022-07-22 14:26:49 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -21,27 +21,3 @@ module.exports = {
   setupFiles: ["<rootDir>/src/setupTests.ts"],
 };
 
-// "jest": {
-//     "testEnvironment": "jsdom",
-//     "preset": "ts-jest",
-//     "rootDir": "src",
-//     "collectCoverageFrom": [
-//       "<rootDir>/**/*.{js,jsx,ts,tsx}"
-//     ],
-//     "coverageDirectory": "jest-coverage",
-//     "transform": {
-//       "^.+\\.tsx?$": "ts-jest"
-//     },
-//     "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-//     "moduleFileExtensions": [
-//       "ts",
-//       "tsx",
-//       "js",
-//       "jsx",
-//       "json",
-//       "node"
-//     ],
-//     "moduleNameMapper": {
-//       "\\.(jpg|jpeg|png|svg|css|less|scss)$": "<rootDir>/__jest__/fileMock.js"
-//     }
-//   }
